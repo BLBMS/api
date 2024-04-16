2b8a3f1ac2a8104d8d7fdad26524c1b8990187b1
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-04-16 21:24:59 +0200

    Create watch.sh

---
+++ watch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+refreshing=60   # refreshing time in sec
+echo "0" >> iteration.txt
+screen -wipe 1>/dev/null 2>&1
+if screen -ls | grep -i Watch;
+then
+  printf "\n\e[93m WATCH is already running! (rwa) to see \e[0m\n"
+  screen -r Watch
+else
+  printf "\n\e[93m Starting WATCH! \e[0m\n"
+  screen -S Watch -X quit 1>/dev/null 2>&1
+  screen -wipe 1>/dev/null 2>&1
+  screen -dmS Watch 1>/dev/null 2>&1
+  screen -S Watch -X stuff "clear \n" 1>/dev/null 2>&1
+  screen -S Watch -X stuff "watch -c -t -n $refreshing './watch-screen.sh'\n" 1>/dev/null 2>&1
+fi
+printf "\n\e[93m opening screen WATCH \e[0m\n"
+printf "\n\e[93m opening with: screen -r Watch or rwa \e[0m\n"
