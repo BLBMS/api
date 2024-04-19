a5fdbf6ad00b6443afcbabdf5227ab7f23f8caea
BLBMS <119702035+BLBMS@users.noreply.github.com>
2024-04-19 19:57:26 +0200

    Update watch-screen.sh

--- watch-screen.sh
+++ watch-screen.sh
@@ -12,6 +12,9 @@ iYellow='\x1B[1;33m'
 C_Off='\x1B[0m'
 ./check-all | jq -c '.[] | [.PHONE,.HOST,.POOL,.MHS]' | sed \
 -e "s/null/${Red}&${C_Off}/g" \
+-e "s/\"0\./${Red}&${Red}/g" \
+-e "s/\"1\./${iRed}&${iRed}/g" \
+-e "s/\"2\./${iYellow}&${iYellow}/g" \
 -e "s/mrr/${Green}&${C_Off}/g" \
 -e "s/de.vipor.net/${Yellow}&${C_Off}/g" \
 -e "s/luckpool/${Blue}&${C_Off}/g" \
